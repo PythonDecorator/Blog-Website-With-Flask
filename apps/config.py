9ed716aa9717078af65412aa5417998bcf6449fe
Okpe <okpeamos.ao@gmail.com>
2023-08-09 00:25:21 +0100

    initial commit

--- apps/config.py
+++ apps/config.py
@@ -18,7 +18,7 @@ class Config(object):
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     # Assets Management
-    ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
+    ASSETS_ROOT = os.getenv('ASSETS_ROOT')
 
     SOCIAL_AUTH_GITHUB = False
 
@@ -39,7 +39,11 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+
+    # # Using the data directly. Not recommended.
+    # This will create a file in <apps> folder.
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data_test.sqlite3')
 
 
 class ProductionConfig(Config):
@@ -61,7 +65,7 @@ class ProductionConfig(Config):
     #     os.getenv('DB_NAME')
     # )
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 
 
 # The different configurations of the script are registered in a config dictionary.
